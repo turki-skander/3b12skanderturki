0a8c12b030022d88d1c3da1b20fbe1fab3cadc86
3b12skanderturki <turki.skander@esprit.tn>
2023-12-02 21:28:00 +0100

    news

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*========================================typing animation ===========*/
+var typed = new Typed(".typing", {
+
+strings:["","web developer ","web designer","informaticien"],
+typeSpeed :100,
+BackSpeed:60,
+loop:true
+
+
+})
